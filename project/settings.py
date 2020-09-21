3a96f5ff1205821b9a8362fb13ff3ccbd167c7e8
atkinsl88 <liamatkins24@gmail.com>
2020-09-21 21:53:20 +0100

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -60,8 +60,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend')]  #Look, we have added the root folder of frontend here
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
